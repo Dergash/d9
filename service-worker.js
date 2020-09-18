9f1ac2fd9c2cc987462fa7ba21a80fc4ddbc3ad6
Dergash <Dergash@users.noreply.github.com>
2020-09-18 06:38:15 +0000

    deploy: 90819245f5da0a25b42aa14f53b542c4df611e82

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e8b928e3e156b5619200ff961f82480a.js"
+  "/d9/precache-manifest.93aea3fbc7c8c1c7612d6cf444761842.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/d9/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
